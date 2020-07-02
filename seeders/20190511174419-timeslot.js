fe746fbc43b983150c628a220ee0d579c27394ec
Juan Carlos Valdés <idevkingos@gmail.com>
2020-07-02 18:40:08 -0300

    arquitectura base

--- seeders/20190511174419-timeslot.js
+++ seeders/20190511174419-timeslot.js
@@ -4,16 +4,22 @@ module.exports = {
     day: 1,
     timeStart: "13:00",
     timeEnd: "19:00",
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }, {
     ProfessionalId: 1,
     day: 3,
     timeStart: "13:00",
     timeEnd: "19:00",
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }, {
     ProfessionalId: 1,
     day: 5,
     timeStart: "16:00",
     timeEnd: "21:00",
+    createdAt: new Date(),
+    updatedAt: new Date(),
   }], {}),
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('TimeSlots', null, {}),
 }
